a11f824c7c2fb653f1e2a4084709c66b1deeda4f
593953149@qq.com <593953149@qq.com>
2018-03-23 00:10:39 +0800

    管理员申请审核页

--- project/sua/urls.py
+++ project/sua/urls.py
@@ -28,12 +28,13 @@ urlpatterns = [
     path('login/', auth.login_view, name='login'),
     path('logout/', auth.logout_view, name='logout'),
     path('test/', login_required(student.TestBaseView.as_view())),
-    
+
     path('publicities/<int:pk>/appeal/', login_required(student.AppealView.as_view())),
     path('applications/apply/', login_required(student.ApplyView.as_view())),
     path('suas/export/download/',login_required(student.Download)),
     path('admin/',login_required(admin.IndexView.as_view())),
     path('admin/appeals/<int:pk>/change/',login_required(admin.AppealView.as_view())),
+    path('admin/applications/<int:pk>/change/',login_required(admin.ApplicationView.as_view())),
 #     path('playMFS/', views.playMFS, name='playMFS'),
 #     path('admin/', views.adminIndex, name='admin-index'),
 #     path('apply_sua/', views.apply_sua, name='apply_sua'),
