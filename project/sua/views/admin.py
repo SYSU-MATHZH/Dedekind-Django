6cf19570e616ce4ef4c3392eb4d3db4cd1eb5e9a
CanDoge <172975487@qq.com>
2018-04-05 09:41:16 +0800

    merge

--- project/sua/views/admin.py
+++ project/sua/views/admin.py
@@ -62,8 +62,8 @@ class IndexView(BaseView, NavMixin):
             context={'request':request}
         )
 
-        appeal_set = Appeal.objects.filter().order_by('created')  # 获取在公示期内的所有公示
-        appeal_data = AppealSerializer(  # 序列化公示
+        appeal_set = Appeal.objects.filter().order_by('created')  # 获取在公示期内的所有申诉
+        appeal_data = AppealSerializer(  # 序列化申诉
             appeal_set,
             many=True,
             context={'request': request}
@@ -77,17 +77,17 @@ class IndexView(BaseView, NavMixin):
             context={'request': request}
         )
 
-        activity_set = Activity.objects.filter()  # 获取所有活动
+        activity_set = Activity.objects.filter(owner=request.user)  # 获取所有当前管理员创建的活动
         activity_data = ActivitySerializer(  # 序列化所有活动
             activity_set,
             many=True,
             context={'request': request}
         )
-        publicity_set = Publicity.objects.filter(
+        publicity_set = Publicity.objects.filter(   # 获取在公示期内的所有公示
             begin__lte=timezone.now(),
             end__gte=timezone.now()
         ).order_by('begin')
-        publicity_data = PublicitySerializer(
+        publicity_data = PublicitySerializer(    # 序列化公示
             publicity_set,
             many=True,
             context={'request': request}
@@ -206,6 +206,53 @@ class ApplicationView(BaseView, NavMixin):
         if serializer.is_valid() and sua_data.is_valid():
             serializer.save(is_checked=True)
             sua_data.save(is_valid=True)
+            self.url = serializer.data['url']
+            return True
+        else:
+            return False
+
+class PublicityView(BaseView, NavMixin):
+    template_name = 'sua/admin_publicity.html'
+    components = {
+        'nav': 'nav',
+    }
+
+    def serialize(self, request, *args, **kwargs):
+        activity_id = kwargs['pk']
+        serialized = super(PublicityView, self).serialize(request)
+
+        activity = Activity.objects.filter(
+            id = activity_id
+            ).get()
+        sua_set = activity.suas.filter(
+            student__number = appeal_data.data['student']['number'],
+            activity__title = appeal_data.data['publicity']['activity']['title'],
+            ).get()
+        sua_data = SuaSerializer(
+            sua_set,
+            context = {'request':request}
+        )
+
+        serializer = AdminAppealSerializer(
+            Appeal.objects.get(id=appeal_id),
+            context={'request':request}
+        )
+        serialized.update({
+            'serializer': serializer,
+            'appeal':appeal_data.data,
+            'sua':sua_data.data,
+        })
+        return serialized
+
+    def deserialize(self, request, *args, **kwargs):
+        appeal_id = kwargs['pk']
+        serializer = AdminAppealSerializer(
+            Appeal.objects.get(id = appeal_id),
+            data=request.data,
+            context={'request': request}
+            )
+        if serializer.is_valid():
+            serializer.save(is_checked=True)
             self.url = serializer.data['url']
             return True
         else:
