66d80f7838e16a68aca00e3299d6e9f6c333852e
Nouzan <clobraindie@outlook.com>
2018-04-20 21:03:50 +0800

    fix some bugs

--- project/sua/views/utils/tools.py
+++ project/sua/views/utils/tools.py
@@ -6,19 +6,31 @@ DATETIME_FORMAT_VALUE = "%Y-%m-%dT%H:%M:%S"
 DATE_FORMAT_VALUE = "%Y-%m-%d"
 
 def TZString2DateTime(s):
+    if s is None:
+        return None
     return dateutil.parser.parse(s)
 
 def TZString2Date(s):
+    if s is None:
+        return None
     return TZString2DateTime(s).date()
 
 def Date2String_SHOW(date):
+    if date is None:
+        return None
     return date.strftime(DATE_FORMAT_SHOW)
 
 def DateTime2String_SHOW(date):
+    if date is None:
+        return None
     return date.strftime(DATETIME_FORMAT_SHOW)
 
 def Date2String_VALUE(date):
+    if date is None:
+        return None
     return date.strftime(DATE_FORMAT_VALUE)
 
 def DateTime2String_VALUE(date):
+    if date is None:
+        return None
     return date.strftime(DATETIME_FORMAT_VALUE)
