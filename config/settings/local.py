96df156ef272134239ab7447ebde3cc4fde0e05a
761516186@qq.com <761516186@qq.com>
2018-04-26 15:40:53 +0800

    setup csrf auth

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,42 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '*',
 ]
+#跨域增加忽略
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
       'localhost:63343',
+      'localhost:8000',
+      'localhost:8001',
+      '127.0.0.1:8000',
+      '127.0.0.1:8001',
+      'http://localhost:63343',
+      'http://localhost:8000',
+      'http://localhost:8001',
+      'http://127.0.0.1:8000',
+      'http://127.0.0.1:8001',
+      'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop'
 )
