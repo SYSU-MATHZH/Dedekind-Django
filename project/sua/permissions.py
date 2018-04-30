9279259592eefee7a1c7bf2124b6330f37e7a766
Demonhero0 <Damon@zhouyuzhedeMacBook-Air.local>
2018-04-30 23:45:41 +0800

    student_index

--- project/sua/permissions.py
+++ project/sua/permissions.py
@@ -41,7 +41,7 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
 class IsAdminUserOrActivity(permissions.BasePermission):
 
     def has_object_permission(self,request,view,obj):
-        print(request.user.student.power)
+#        print(request.user.student.power)
         if not (request.user and request.user.is_authenticated):
             return False
         if (request.user.is_staff or request.user.student.power == 1):
