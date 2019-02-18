352d6684a974af0160e3c26f46c350cd90bf0bc7
HareInWeed <34718939+HareInWeed@users.noreply.github.com>
2019-02-18 10:34:44 +0800

    fix conflicts

--- project/sua/admin.py
+++ project/sua/admin.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from project.sua.models import *
-
-
-admin.site.register(Sua)
-admin.site.register(Student)
-admin.site.register(Proof)
-admin.site.register(Application)
-admin.site.register(SuaGroup)
-admin.site.register(Activity)
-admin.site.register(Publicity)
-admin.site.register(Appeal)
-=======
 from django.contrib import admin
 from project.sua.models import *
 
@@ -25,4 +11,3 @@ admin.site.register(Activity)
 admin.site.register(Publicity)
 admin.site.register(Appeal)
 admin.site.register(AcademicYear)
->>>>>>> 4aeedf022c269cdd7612b09a4c4b063cbcb5fdf6
--- project/sua/views/student/serializers.py
+++ project/sua/views/student/serializers.py
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-from rest_framework import serializers
-from project.sua.models import Activity, Proof, Sua, Application
-
-
-class DEActivityForAddApplicationsSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Activity
-        fields = ('url','title', 'detail', 'group', 'date')
-
-
-class DESuaForAddApplicationsSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Sua
-        fields = ('url','team', 'suahours')
-
-
-class DEProofForAddApplicationsSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Proof
-        fields = ('url','is_offline', 'proof_file')
-
-
-class DEAddApplicationsSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Application
-        fields = ('url', 'contact')
-=======
 from rest_framework import serializers
 from project.sua.models import Activity, Proof, Sua, Application
 
@@ -60,4 +28,3 @@ class DEAddApplicationsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Application
         fields = ('url', 'contact')
->>>>>>> 4aeedf022c269cdd7612b09a4c4b063cbcb5fdf6
